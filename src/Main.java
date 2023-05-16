c62bc816722df9ba85eca8de2cad4a52d867847e
Katrin Valge <katrin.valge@mail.ee>
2023-05-16 19:40:37 +0300

    Created main class.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import database.CreateTableQuery;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CreateTableQuery createTableQuery = new CreateTableQuery();
+        createTableQuery.addTables();
+    }
+}
