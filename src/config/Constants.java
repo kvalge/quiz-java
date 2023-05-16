b8d4bd9daee6216db354fb1d638cc19b185f966e
Katrin Valge <katrin.valge@mail.ee>
2023-05-16 21:06:33 +0300

    Formatting.

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -8,7 +8,6 @@ public class Constants {
     public static final String TABLE_QUIZ = "quiz";
     public static final String QUERY_FAILED = "Query failed: ";
 
-
     public static final int RANK_1 = 1;
     public static final int RANK_2 = 2;
     public static final int RANK_3 = 3;
