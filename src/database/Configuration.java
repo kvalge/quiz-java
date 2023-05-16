a2a4fca365e210b31e8402d957d9dd7d3c637978
Katrin Valge <katrin.valge@mail.ee>
2023-05-16 19:12:42 +0300

    Created database configuration class.

---
+++ src/database/Configuration.java
@@ -0,0 +1,25 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Configuration {
+
+    public static final String DATABASE_NAME = "postgres";
+    public static final String URL = "jdbc:postgresql://localhost:5432/" + DATABASE_NAME;
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "postgres";
+
+    public Connection connect() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            conn.setAutoCommit(true);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
