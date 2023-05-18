cca7c91036824b61645f304a31c667ffec758168
Katrin Valge <katrin.valge@mail.ee>
2023-05-18 16:46:30 +0300

    Updated id variables.

--- src/dao/DaoQuestion.java
+++ src/dao/DaoQuestion.java
@@ -42,21 +42,25 @@ public class DaoQuestion {
             "Which country was known as Rhodesia before gaining independence from the British in 1979?",
             RANK_1,
             idT1);
+    long q1Id = Question.getId();
 
     Question q2 = new Question(
             "Which country was unified by Giuseppe Garibaldi in 1851?",
             RANK_2,
             idT1);
+    long q2Id = Question.getId();
 
     Question q3 = new Question(
             "Which President was brought down by the Watergate Scandal?",
             RANK_3,
             idT1);
+    long q3Id = Question.getId();
 
     Question q4 = new Question(
             "Who directed Lost in Translation?",
             RANK_3,
             idT2);
+    long q4Id = Question.getId();
 
     public void addNewQuestion() {
         try {
@@ -64,20 +68,20 @@ public class DaoQuestion {
 
             statement.executeUpdate(
                     "INSERT INTO " + TABLE_QUESTION + "(id, content, rank, " + TABLE_TOPIC + "_id)" +
-                            "VALUES (" + Question.getId() + ", '" + q1.getContent() + "', " + q1.getRank()
+                            "VALUES (" + q1Id + ", '" + q1.getContent() + "', " + q1.getRank()
                             + ", '" + q1.getTopicId() + "')");
             statement.executeUpdate(
                     "INSERT INTO " + TABLE_QUESTION + "(id, content, rank, " + TABLE_TOPIC + "_id)" +
-                            "VALUES (" + Question.getId() + ", '" + q2.getContent() + "', " + q2.getRank()
+                            "VALUES (" + q2Id + ", '" + q2.getContent() + "', " + q2.getRank()
                             + ", '" + q2.getTopicId() + "')");
             statement.executeUpdate(
                     "INSERT INTO " + TABLE_QUESTION + "(id, content, rank, " + TABLE_TOPIC + "_id)" +
-                            "VALUES (" + Question.getId() + ", '" + q3.getContent() + "', " + q3.getRank()
+                            "VALUES (" + q3Id + ", '" + q3.getContent() + "', " + q3.getRank()
                             + ", '" + q3.getTopicId() + "')");
             statement.executeUpdate(
                     "INSERT INTO " + TABLE_QUESTION + "(id, content, rank, " + TABLE_TOPIC + "_id)" +
-                            "VALUES (" + Question.getId() + ", '" + q4.getContent() + "', " + q4.getRank()
-                            + ", '" + q4.getTopicId() + "') ON CONFLICT (content) DO NOTHING");
+                            "VALUES (" + q4Id + ", '" + q4.getContent() + "', " + q4.getRank()
+                            + ", '" + q4.getTopicId() + "')");
         } catch (SQLException e) {
             System.out.println(QUERY_FAILED + e.getMessage());
             e.getStackTrace();
@@ -103,7 +107,7 @@ public class DaoQuestion {
         try {
             Statement statement = configuration.connect().createStatement();
 
-            statement.executeUpdate("UPDATE " + TABLE_QUESTION + " SET rank = " + RANK_1 + " WHERE id = 3");
+            statement.executeUpdate("UPDATE " + TABLE_QUESTION + " SET rank = " + RANK_1 + " WHERE id = 4");
         } catch (SQLException e) {
             System.out.println(QUERY_FAILED + e.getMessage());
             e.getStackTrace();
@@ -114,16 +118,19 @@ public class DaoQuestion {
         try {
             Statement statement = configuration.connect().createStatement();
 
-            statement.execute("DELETE FROM " + TABLE_QUESTION + " WHERE id = 1");
+            statement.execute("DELETE FROM " + TABLE_QUESTION + " WHERE id = 4");
         } catch (SQLException e) {
             System.out.println(QUERY_FAILED + e.getMessage());
             e.getStackTrace();
         }
     }
 
-    Response r1q1 = new Response("India", false, Question.getId());
-    Response r2q1 = new Response("Zimbabwe", true, Question.getId());
-    Response r3q1 = new Response("Sierra-Leone", false, Question.getId());
+    Response r1q1 = new Response("India", false, q1Id);
+    Response r2q1 = new Response("Zimbabwe", true, q1Id);
+    Response r3q1 = new Response("Sierra-Leone", false, q1Id);
+    Response r1q2 = new Response("Spain", false, q2Id);
+    Response r2q2 = new Response("Switzerland", false, q2Id);
+    Response r3q2 = new Response("Italy", true, q2Id);
 
     public void addNewResponse() {
         try {
@@ -136,14 +143,42 @@ public class DaoQuestion {
             statement.executeUpdate(
                     "INSERT INTO " + TABLE_RESPONSE + "(id, content, correct, " + TABLE_QUESTION + "_id)" +
                             "VALUES (" + Response.getId() + ", '" + r2q1.getContent() + "', " + r2q1.getCorrect()
-                            + ", '" + r1q1.getQuestionId() + "')");
+                            + ", '" + r2q1.getQuestionId() + "')");
             statement.executeUpdate(
                     "INSERT INTO " + TABLE_RESPONSE + "(id, content, correct, " + TABLE_QUESTION + "_id)" +
                             "VALUES (" + Response.getId() + ", '" + r3q1.getContent() + "', " + r3q1.getCorrect()
-                            + ", '" + r1q1.getQuestionId() + "')");
+                            + ", '" + r2q1.getQuestionId() + "')");
+            statement.executeUpdate(
+                    "INSERT INTO " + TABLE_RESPONSE + "(id, content, correct, " + TABLE_QUESTION + "_id)" +
+                            "VALUES (" + Response.getId() + ", '" + r1q2.getContent() + "', " + r1q2.getCorrect()
+                            + ", '" + r1q2.getQuestionId() + "')");
+            statement.executeUpdate(
+                    "INSERT INTO " + TABLE_RESPONSE + "(id, content, correct, " + TABLE_QUESTION + "_id)" +
+                            "VALUES (" + Response.getId() + ", '" + r2q2.getContent() + "', " + r2q2.getCorrect()
+                            + ", '" + r2q2.getQuestionId() + "')");
+            statement.executeUpdate(
+                    "INSERT INTO " + TABLE_RESPONSE + "(id, content, correct, " + TABLE_QUESTION + "_id)" +
+                            "VALUES (" + Response.getId() + ", '" + r3q2.getContent() + "', " + r3q2.getCorrect()
+                            + ", '" + r3q2.getQuestionId() + "')");
         } catch (SQLException e) {
             System.out.println(QUERY_FAILED + e.getMessage());
             e.getStackTrace();
         }
     }
+
+/*    Quiz quiz1 = new Quiz(Question.getId());
+
+    public void addNewQuiz() {
+        try {
+            Statement statement = configuration.connect().createStatement();
+
+            statement.executeUpdate(
+                    "INSERT INTO " + TABLE_QUIZ + "(id" + TABLE_QUESTION + "_id)" +
+                            "VALUES (" + Quiz.getId() + ", '" + r1q1.getQuestionId() + "')");
+        } catch (SQLException e) {
+            System.out.println(QUERY_FAILED + e.getMessage());
+            e.getStackTrace();
+        }
+    }*/
+
 }
